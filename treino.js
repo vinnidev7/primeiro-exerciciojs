a27922348efbbc96978c7bce1655acb1eddde13d
vinicius hans <vinizinsantos888@gmail.com>
2025-10-17 04:33:29 -0300

    Add files via upload

---
+++ treino.js
@@ -0,0 +1,37 @@
+// estudo dia 17/10/25  inicio: seção 3 cap : 23 ;
+let quantity = 10;
+let price = 6.4;
+let tax  = 1.5;
+let FinalValue = price + tax; // preco  =  imposto;
+const Name = "Pen";
+
+
+console.log(Name);
+console.log("Quantity: " + quantity);
+console.log("price: "+ price + " R$");
+console.log("tax: " + tax + " R$");
+console.log("Final Value: " + FinalValue + " R$");
+
+
+// ate aqui exercicio todo ok ;
+
+
+// desafio calcular área de uma circunferencia 
+
+ const PI = 3.14159;
+ const raio = 10;
+ let Finalvalue = PI * raio * raio;
+
+
+ console.log("Valor de PI: "+ PI );
+ console.log("Valor do raio: " + raio);
+ console.log("Valor final da circunferência: " + Finalvalue + "m2");
+
+ // exercicio 2 ok 100%;
+
+
+
+
+
+
+
