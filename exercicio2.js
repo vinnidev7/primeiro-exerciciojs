1d4e2458bdb7fb74849923dc6bdda0e124d56306
emerson <emersonlsg10@gmail.com>
2025-10-20 21:31:50 -0300

    fiz o exercicio 2

---
+++ exercicio2.js
@@ -0,0 +1,14 @@
+// Leia dois números e mostre a soma.
+
+let numero1 = 10
+let numero2 = 20
+
+// console.log('a soma dos numeros é: ', numero1 + numero2)
+
+function somaValores(){ // aqui eu declaro a função
+    let resultado = numero1 + numero2;
+
+    console.log('a soma dos numeros é ', resultado)
+}
+
+somaValores() // aqui eu executo a função
