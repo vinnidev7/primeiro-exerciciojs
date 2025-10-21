200ef75330617218450378573f8f9eb96bab65d2
emerson <emersonlsg10@gmail.com>
2025-10-20 21:23:50 -0300

    fiz o exercicio 1

---
+++ exercicio1.js
@@ -0,0 +1,11 @@
+// Leia um número e mostre o dobro dele.
+
+const numero = 3
+
+function calculaDobro(){ // aqui eu declaro a função
+    const resultado = numero * 2;
+
+    console.log('o dobro é: ', resultado)
+}
+
+calculaDobro() // aqui eu executo a função
--- exercicios.js
+++
@@ -1,1 +0,0 @@
-console.log("Exercícios de JavaScript");
