1db0ab789c5e41e242831eec26c5b2326d02ebe7
emerson <emersonlsg10@gmail.com>
2025-10-20 21:37:39 -0300

    fiz o exercicio 13

---
+++ exercicio13.js
@@ -0,0 +1,12 @@
+// Calcule a raiz quadrada de um número.
+let numero = 100
+
+// console.log('a raiz quadrada é: ', Math.sqrt(numero))
+
+function calculaRaizQuadrada(){ // aqui eu declaro a função
+    let resultado = Math.sqrt(numero)
+
+    console.log('a raiz quadrada é: ', resultado)
+}
+
+calculaRaizQuadrada() // aqui eu to mandando ela executar
